05fe117078f0353f56f9e2cc237c89801b469fd1
Josh Pollock <jpollock412@gmail.com>
2022-01-07 16:04:01 -0500

    bind services

--- inc/functions.php
+++ inc/functions.php
@@ -1,2 +1,24 @@
-<?php 
+<?php
 /** Functions **/
+
+function candle(){
+    static $candle;
+    if( ! $candle ){
+        $candle = new \Candles\Plugin();
+        do_action( "candle", $candle );
+    }
+    return $candle;
+}
+
+add_action( 'candle', function($candle){
+    $api = new \Candles\ApiClient(
+        'key'
+    );
+    $candle->instance(\Candles\ApiClient::class, $api);
+});
+
+add_action( 'candle', function($candle){
+    $candle->bind( \Candles\Product::class, function($plugin,$args){
+        return new \Candles\Product(...$args);
+    });
+});
---
+++ tests/Integration/ContainerTest.php
@@ -0,0 +1,41 @@
+<?php
+namespace Candles\Tests\Integration;
+
+use PHPUnit\Framework\TestCase;
+
+use Candles\Plugin;
+use Candles\ApiClient;
+use Candles\Product;
+
+class ContainerTest extends TestCase {
+
+    public function testAccesorFunction(){
+        $this->assertInstanceOf(
+            Plugin::class,
+            candle()
+        );
+        $this->assertSame(
+            candle(),
+            candle()
+        );
+    }
+
+    public function testApiClient() {
+        $this->assertInstanceOf(
+            ApiClient::class,
+            candle()->make(ApiClient::class)
+        );
+
+    }
+
+    public function testProduct(){
+        $this->assertInstanceOf(
+            Product::class,
+            candle()->make(Product::class,['name'])
+        );
+        $this->assertEquals(
+            'test2',
+            candle()->make(Product::class,['test2'])->name
+        );
+    }
+}
